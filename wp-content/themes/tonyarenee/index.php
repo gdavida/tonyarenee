9a4ea51a27953dc392437694e4b8d74a84249621
Davida Gaffney <gdavida@gmail.com>
2016-05-17 20:55:40 -0500

    index styling for responsiveness

--- wp-content/themes/tonyarenee/index.php
+++ wp-content/themes/tonyarenee/index.php
@@ -54,7 +54,7 @@
     <div class="container-fluid">
 
     <!-- HEADER NAME W LOGO -->
-    <div class="row header">
+    <div class="row header greyBkgd">
       <div class="col-xs-12 ">
         <img class="circle trLogo" src="http://localhost:8888/tonyarenee/wp-content/themes/tonyarenee/images/logoTeal.png">
       </div>
@@ -76,8 +76,8 @@
 
 
       <!-- TOP QUOTE SECTION -->
-    <div class="row whiteBkgd quoteTop whiteTriangleDown">
-      <div class="col-xs-12 arrowBox" >
+    <div class="row whiteBkgd quoteTop">
+      <div class="col-xs-12 arrowDownBoxWhite" >
         <h4 class="quotes">“To change the world...
         <span class="break"><br></span>
         start with one step<br>And however small...
@@ -86,7 +86,7 @@
     </div>
 
       <!-- FEATURE VIDEO -->
-    <div class="row">
+    <div class="row greyBkgd">
       <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 featuredVideoPost">
         <img class="img-responsive" src="http://localhost:8888/tonyarenee/wp-content/themes/tonyarenee/images/skiBwTeal.jpg">
         <h3>Tonya Time on Tuesdays</h3>
@@ -96,13 +96,13 @@
 
       <!-- MIDDLE QUOTE SECTION -->
     <div class="row whiteBkgd">
-      <div class="col-xs-12 quoteMiddle">
+      <div class="col-xs-12 quoteMiddle arrowDownBoxWhite">
         <h4 class="quotes">A simple Hello could lead<br>to a million things.</h4>
       </div>
     </div>
 
       <!-- WELCOME MESSAGE -->
-    <div class="row">
+    <div class="row greyBkgd">
       <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 welcomeMessage">
         <img class="img-responsive" src="http://localhost:8888/tonyarenee/wp-content/themes/tonyarenee/images/pierBwTeal.jpg">
         <h3>Hi Friends</h3>
@@ -111,13 +111,13 @@
 
       <!-- BOTTOM QUOTE SECTION -->
     <div class="row whiteBkgd">
-      <div class="col-xs-12 quoteBottom">
+      <div class="col-xs-12 quoteBottom arrowDownBoxWhite">
         <h4 class="quotes">“Happiness is not a state to arrive at,<br>but a manner of travelling.”<br>~Margaret L. Runbeck</h4>
       </div>
     </div>
 
       <!-- FEATURE TRAVEL DESTINATION -->
-    <div class="row featuredTravel">
+    <div class="row featuredTravel greyBkgd">
       <div class="col-xs-10 col-xs-offset-1">
         <h3>Featured</h3>
       </div>
@@ -137,7 +137,7 @@
     </div>
 
       <!-- TRAVEL POSTS -->
-    <div class="travelPosts">
+    <div class="travelPosts greyBkgd">
         <div class="row travel1">
           <div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-7">
             <img class="img-responsive" src="http://localhost:8888/tonyarenee/wp-content/themes/tonyarenee/images/timeline1.jpg">
@@ -195,7 +195,7 @@
       </div>
 
       <!-- JOIN THE JOURNEY SECTION -->
-    <div class="row joinTheJourney">
+    <div class="row joinTheJourney greyBkgd">
       <div class="col-xs-12">
         <div class="journeyText">
           <h1>Join the Journey</h1>
@@ -208,7 +208,7 @@
     </div>
 
       <!-- JOIN VIP LIST -->
-    <div class="row bottomSection">
+    <div class="row bottomSection greyBkgd">
       <div class="col-xs-12 col-sm-4 col-sm-offset-1 pull-right joinTheVIPList">
         <h3>Join the VIP list</h3>
       </div>
