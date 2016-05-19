62970e9f992cd5001ce1f77047294136da91bfdf
Davida Gaffney <gdavida@gmail.com>
2016-05-19 14:23:51 -0500

    fixed responsive style issue with header

---
+++ wp-content/themes/tonyarenee/page-articles.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage TonyaRenee
+ */
+
+get_header(); ?>
+
+
+      <!-- TITLE SECTION -->
+    <div class="row whiteBkgd title">
+      <div class="col-xs-12 arrowDownBoxWhite" >
+        <h3 class="title">Recent Posts</h3>
+      </div>
+    </div>
+
+
+      <!-- TITLE SECTION -->
+    <div class="row whiteBkgd recentPosts">
+      <div class="col-xs-4 recent1">
+        <img class="img-responsive" src="http://www.lorempixel.com/400/300/">
+        <h4 class="recentPostTitle">Title Here</h4>
+        <p class="recentPostReadMore">Title Here</p>
+        <p class="recentPostDate">May 5, 2016</p>
+      </div>
+      <div class="col-xs-4 recent2">
+        <img class="img-responsive" src="http://www.lorempixel.com/400/300/">
+        <h4 class="recentPostTitle">Title Here</h4>
+        <p class="recentPostReadMore">Title Here</p>
+        <p class="recentPostDate">May 5, 2016</p>
+      </div>
+      <div class="col-xs-4 recent3">
+        <img class="img-responsive" src="http://www.lorempixel.com/400/300/">
+        <h4 class="recentPostTitle">Title Here</h4>
+        <p class="recentPostReadMore">Title Here</p>
+        <p class="recentPostDate">May 5, 2016</p>
+      </div>
+    </div>
+
+      <!-- MAIN ARTICLE SECTION -->
+    <div class="row recentPostFeature whiteBkgd">
+      <div class="col-xs-12 col-sm-5 title">
+        <h5 class="sectionTitle">Featured Article</h5>
+        <h1 class="articleTitle">Title of Feature Here</h1>
+      </div>
+      <div class="col-xs-12 col-sm-7 featuredImage">
+        <img class="img-responsive" src="http://www.lorempixel.com/600/450/people">
+      </div>
+    </div>
+
+
+   
+<?php 
+  get_footer();
