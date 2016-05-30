7fcad1ef4e42762452c3a1cf756b692f8901f61e
Davida Gaffney <gdavida@gmail.com>
2016-05-30 17:47:37 -0500

    article loop working

--- wp-content/themes/tr_s/page-articles.php
+++ wp-content/themes/tr_s/page-articles.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage TonyaRenee
+ */
+
 get_header(); ?>
 
 
@@ -26,34 +37,48 @@ get_header(); ?>
           </li>
         </ul>
       </nav>
-      <div class="col-xs-4 recent1">
-        <img class="img-responsive" src="http://www.lorempixel.com/400/300/">
-        <h4 class="recentPostTitle">Title Here</h4>
-        <p class="recentPostDate">May 5, 2016</p>
-      </div>
-      <div class="col-xs-4 recent2">
-        <img class="img-responsive" src="http://www.lorempixel.com/400/300/">
-        <h4 class="recentPostTitle">Title Here</h4>
-        <p class="recentPostDate">May 5, 2016</p>
-      </div>
-      <div class="col-xs-4 recent3">
-        <img class="img-responsive" src="http://www.lorempixel.com/400/300/">
-        <h4 class="recentPostTitle">Title Here</h4>
-        <p class="recentPostDate">May 5, 2016</p>
-      </div>
+          <!-- first loop for featured Article loop -->
+      <?php $query1 = new WP_Query( array( 'category_name' => 'Article', 'posts_per_page' => 3, 'offset' => 1 ) ); ?>
+
+      <?php while ( $query1->have_posts() ) : $query1->the_post(); ?>
+
+        <div class="col-xs-4 recent1">
+          <img src="<?php the_field('image'); ?>" class="img-responsive">
+          <h4 class="recentPostTitle"><?php the_field('title'); ?></h4>
+          <p class="recentPostDate"><?php the_field('article_publish_date'); ?></p>
+        </div>
+
+
+      <?php endwhile; ?>
+
+      <?php wp_reset_postdata(); ?>
+  <!-- end of the featured article loop -->
+
+
     </div>
+
+
+      <!-- second loop for featured Article loop -->
+      <?php $query2 = new WP_Query( array( 'category_name' => 'Article', 'posts_per_page' => 1) ); ?>
+
+      <?php while ( $query2->have_posts() ) : $query2->the_post(); ?>
 
       <!-- MAIN ARTICLE SECTION -->
     <div class="row recentPostFeature whiteBkgd">
       <div class="col-xs-12 col-sm-5 title">
-        <h5 class="sectionTitle">Featured Article</h5>
-        <h1 class="articleTitle">Title of Feature Here</h1>
+        <h5 class="articleTagline"><?php the_field('article_tagline'); ?></h5>
+        <h1 class="articleTitle"><?php the_field('title'); ?></h1>
       </div>
       <div class="col-xs-12 col-sm-7 featuredImage">
-        <img class="img-responsive" src="http://www.lorempixel.com/600/450/people">
+        <img class="img-responsive" src="<?php the_field('image'); ?>">
       </div>
     </div>
 
+
+      <?php endwhile; ?>
+
+      <?php wp_reset_postdata(); ?>
+  <!-- end of the featured article loop -->
 
    
 <?php 
