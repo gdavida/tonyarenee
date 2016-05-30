872506c103272787476ba034477792572cc27466
Davida Gaffney <gdavida@gmail.com>
2016-05-30 16:02:42 -0500

    commit

--- wp-content/themes/tr_s/page-video.php
+++ wp-content/themes/tr_s/page-video.php
@@ -11,36 +11,6 @@
  */
 
 get_header(); ?>
-<?php $query1 = new WP_Query( array( 'category_name' => 'Video', 'posts_per_page' => 1 ) ); ?>
-
-<?php if ( $query1->have_posts() ) : ?>
-
-  <!-- pagination here -->
-
-  <!-- the loop -->
-  <?php while ( $query1->have_posts() ) : $query1->the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-  <?php endwhile; ?>
-  <!-- end of the loop -->
-
-  <!-- pagination here -->
-
-  <?php wp_reset_postdata(); ?>
-
-  <?php /* The 2nd Query (without global var) */ ?>
-  <?php $query2 = new WP_Query( array( 'category_name' => 'Video', 'posts_per_page' => 6, 'offset' => 1) ); ?>
-
-  <?php // The 2nd Loop
-  while ( $query2->have_posts() ) {
-    $query2->the_post();
-    echo '<li>' . get_the_title( $query2->post->ID ) . '</li>';
-  } ?>
-
-
-<?php else : ?>
-  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-
 
       <!-- TOP QUOTE SECTION -->
     <div class="row tealBkgd title">
@@ -49,10 +19,43 @@ get_header(); ?>
       </div>
     </div>
 
+<!-- end of the featured video loop -->
+
+
+
+<!-- first loop for featured Video loop -->
+<?php $query1 = new WP_Query( array( 'category_name' => 'Video', 'posts_per_page' => 1 ) ); ?>
+
+<?php if ( $query1->have_posts() ) : ?>
+
+  <?php while ( $query1->have_posts() ) : $query1->the_post(); ?>
+    <h2><?php the_field('description'); ?></h2>
+
+
+
+  <?php endwhile; ?>
+
+  <?php wp_reset_postdata(); ?>
       <!-- MAIN VIDEO SECTION -->
     <div class="row mainVideo greyBkgd">
         <h3>Watch Latest</h3>
     </div>
+
+
+
+
+<!-- 2nd loop for recent 6 loop -->
+  <?php /* The 2nd Query (without global var) */ ?>
+  <?php $query2 = new WP_Query( array( 'category_name' => 'Video', 'posts_per_page' => 6, 'offset' => 1) ); ?>
+
+  <?php
+  while ( $query2->have_posts() ) {
+    $query2->the_post();
+    echo '<li>' . get_the_title( $query2->post->ID ) . '</li>';
+  } ?>
+<!-- end of the recent 6 loop -->
+
+
     <div class="row recentVideos whiteBkgd">
       <div class="col-xs-12 col-sm-4 col-sm-offset-4">
         <h4>Recent Videos</h4>
@@ -61,24 +64,14 @@ get_header(); ?>
         <div class="col-xs-6 col-sm-4 col-sm-2 recentVideoPost1">
           <img class="img-responsive" src="http://www.lorempixel.com/150/150/abstract/1">
         </div>
-        <div class="col-xs-6 col-sm-4 col-sm-2 recentVideoPost2">
-          <img class="img-responsive" src="http://www.lorempixel.com/150/150/abstract/2">
-        </div>
-        <div class="col-xs-6 col-sm-4 col-sm-2 recentVideoPost3">
-          <img class="img-responsive" src="http://www.lorempixel.com/150/150/abstract/3">
-        </div>
-        <div class="col-xs-6 col-sm-4 col-sm-2 recentVideoPost4">
-          <img class="img-responsive" src="http://www.lorempixel.com/150/150/abstract/4">
-        </div>
-        <div class="col-xs-6 col-sm-4 col-sm-2 recentVideoPost5">
-          <img class="img-responsive" src="http://www.lorempixel.com/150/150/abstract/5">
-        </div>
-        <div class="col-xs-6 col-sm-4 col-sm-2 recentVideoPost6">
-          <img class="img-responsive" src="http://www.lorempixel.com/150/150/abstract/6">
-        </div>
+
       </div>
     </div>
 
+<!-- end of the multiloop if there's nothing that meets criteria -->
+<?php else : ?>
+  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
    
 <?php 
